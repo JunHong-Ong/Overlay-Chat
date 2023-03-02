d69438a7524bcbcf813b801b9ac7e4998b0b6ec3
Ong Jun Hong <53672346+JunHong-Ong@users.noreply.github.com>
2023-03-02 23:12:38 +0800

    Started implementation
    Able to receive messages from twitch websocket

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,58 +1,53 @@
-var popout_chat;
+var overlayChat;
 const vidContainer = document.querySelector(".video-player__overlay");
 const broadcaster = document.URL.match("(?:https://www.twitch.tv/)(?<broadcaster>.+)").groups["broadcaster"];
 
-class PopOutChat {
-    constructor(posX = 0, posY = 0, height = 300, width = 200, visible = true) {
-        this.posX = posX;
-        this.posY = posY;
-        this.height = height;
-        this.width = width;
-        this.visible = visible;
-        this.broadcaster = broadcaster;
+class OverlayChat {
+    constructor(broadcaster, position=[0, 0], size=[300, 200]) {
+        // (X, Y) position of overlay chat within the video container.
+        [this._posX, this._posY] = position;
+        // Height and Width of overlay chat.
+        [this._height, this._width] = size;
 
-        this.generate();
+        // Meta properties of the overlay chat.
+        this.broadcaster = broadcaster;
+        this.isVisible = true;
+        
+        // Generate and inserts draggable element into video container
+        this._generate();
+        this._insertElement(this.element, vidContainer);
 
         // Make the DIV element draggable:
         dragElement(this.element);
     }
 
-    generate() {
-        this.element = document.createElement("div");
-        this.element.id = "pop-out-chat";
-        this.element.textContent = "Chat goes here..."; // Remove in future
-
-        vidContainer.insertAdjacentElement("afterbegin", this.element);
-        this.parent = this.element.parentElement;
-    }
-
     update() {
-        if (this.visible) {
-            console.log("Showing element.");
+        if (this.isVisible) {
+            console.info("Showing element.");
             this.element.removeAttribute("hidden");
         } else {
-            console.log("Hiding element.");
+            console.info("Hiding element.");
             this.element.setAttribute("hidden", true);
         }
 
         this.checkPosition();
 
-        this.element.style.top = this.posY + "px";
-        this.element.style.left = this.posX + "px";
-        this.element.style.height = this.height + "px";
-        this.element.style.width = this.width + "px";
+        this.element.style.top = this._posY + "px";
+        this.element.style.left = this._posX + "px";
+        this.element.style.height = this._height + "px";
+        this.element.style.width = this._width + "px";
 
         this.updateSettings();
     }
 
     checkPosition() {
-        this.posX = this.posX < 0 ? 0 : this.posX;
-        this.posY = this.posY < 0 ? 0 : this.posY;
+        this._posX = this._posX < 0 ? 0 : this._posX;
+        this._posY = this._posY < 0 ? 0 : this._posY;
 
-        this.posX = this.posX > this.parent.offsetWidth - this.width ?
-            this.parent.offsetWidth - this.width : this.posX;
-        this.posY = this.posY > this.parent.offsetHeight - this.height ?
-            this.parent.offsetHeight - this.height : this.posY;
+        this._posX = this._posX > this.element.parent.offsetWidth - this._width ?
+            this.element.parent.offsetWidth - this._width : this._posX;
+        this._posY = this._posY > this.element.parent.offsetHeight - this._height ?
+            this.element.parent.offsetHeight - this._height : this._posY;
     }
 
     updateSettings() {
@@ -61,29 +56,47 @@ class PopOutChat {
             resource: "settings",
             data: {
                 name: this.broadcaster,
-                visible: this.visible,
-                position: [this.posX, this.posY, this.height, this.width]
+                visible: this.isVisible,
+                position: [this._posX, this._posY, this._height, this._width]
             }
         });
     }
 
     get position() {
-        return [this.posX, this.posY];
+        return [this._posX, this._posY];
     }
 
     set position(value) {
-        [this.posX, this.posY] = value;
-        console.info(`Position has been updated to (${this.posX}, ${this.posY}).`);
+        [this._posX, this._posY] = value;
+        console.info(`Position has been updated to (${this._posX}, ${this._posY}).`);
     }
 
     get size() {
-        return [this.height, this.width];
+        return [this._height, this._width];
     }
 
     set size(value) {
-        [this.height, this.width] = value;
-        console.info(`Size has been updated to (${this.height}, ${this.width}).`);
+        [this._height, this._width] = value;
+        console.info(`Size has been updated to (${this._height}, ${this._width}).`);
     }
+
+    _generate() {
+        this.element = document.createElement("div");
+        this.element.id = "overlay-chat";
+        this.element.textContent = "Chat goes here..."; // Remove in future
+    }
+
+    /**
+     * Inserts an element as the child of another element.
+     * 
+     * @param {*} child 
+     * @param {*} parent 
+     */
+    _insertElement(child, parent) {
+        parent.insertAdjacentElement("afterbegin", child);
+        child.parent = parent;
+    }
+    
 };
 
 function dragElement(elmnt) {
@@ -133,8 +146,8 @@ function dragElement(elmnt) {
         document.onmouseup = null;
         document.onmousemove = null;
         
-        popout_chat.position = [elmnt.offsetLeft, elmnt.offsetTop];
-        popout_chat.update();
+        overlayChat.position = [elmnt.offsetLeft, elmnt.offsetTop];
+        overlayChat.update();
     }
 };
 
@@ -145,29 +158,34 @@ function main() {
     settings.then((response) => {
         if (response.statusCode === 422) {
             console.info("Generating default popout.")
-            popout_chat = new PopOutChat();
+            overlayChat = new OverlayChat();
         } else if (response.statusCode === 200) {
             console.info("Generating popout from saved settings.")
             var posX, posY, height, width, visible;
             [posX, posY, height, width] = response.content.position;
             visible = response.content.visible;
-            popout_chat = new PopOutChat(posX, posY, height, width, visible);
+            overlayChat = new OverlayChat(posX, posY, height, width, visible);
         }
 
-        popout_chat.update();
-        new ResizeObserver(() => {popout_chat.update()}).observe(vidContainer);
-    });
+        overlayChat.update();
+        new ResizeObserver(() => {overlayChat.update()}).observe(vidContainer);
 
+        var port = chrome.runtime.connect({name: broadcaster});
+
+        port.onMessage.addListener((message) => {
+            overlayChat.element.textContent = message.data;
+        });
+    });
 };
 
 chrome.runtime.onMessage.addListener((response) => {
     if (response === "show chat") {
-        popout_chat.visible = true;
+        overlayChat.visible = true;
     } else if (response === "hide chat") {
-        popout_chat.visible = false;
+        overlayChat.visible = false;
     }
 
-    popout_chat.update();
+    overlayChat.update();
 })
 
 main();
